3c68870257048ad967d8cdd1cb4a84e78115ae08
Ty Everton <everton_915@yahoo.com>
2021-06-28 14:56:48 -0600

    math function tests

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const getGreeting = require('./project');
 
+const { expect, test, describe } = require('@jest/globals')
+
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
@@ -8,6 +10,16 @@ const add = function(num1, num2) {
   return num1 + num2
 }
 
-test('can add numbers', function() {
-  expect(add(1, 5)).toEqual(6)
+const multiply = function(num1, num2) {
+  return num1 * num2
+}
+
+describe('math functions', function() {
+  test('can add numbers', function() {
+    expect(add(1, 5)).toEqual(6)
+  })
+  test('can multiply numbers', function() {
+    expect(multiply(1, 5)).toEqual(5)
+  })
 })
+
