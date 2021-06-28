d9607e3662e9925a10a51ce5c98c5dc32b91616f
Ty Everton <everton_915@yahoo.com>
2021-06-28 14:39:34 -0600

    added addition function

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+const add = function(num1, num2) {
+  return num1 + num2
+}
+
+test('can add numbers', function() {
+  expect(add(1, 5)).toEqual(6)
+})
